dca663cda90ccef9d005300d581552b1bddc10b4
Arthur Duchene <arthurduchene025@gmail.com>
2025-11-29 23:02:35 +0100

    Initial commit from Xcode project

---
+++ SwiftcordPlus/SwiftcordPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftcordPlusApp.swift
+//  SwiftcordPlus
+//
+//  Created by Arthur Duchene on 29/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftcordPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
